6b1e7416459cb7f4a54f34bbc87387d19afebc2e
Myles Spencer <mspencer.mes@gmail.com>
2018-02-09 17:59:53 -0500

    Created file and added argument parsing

---
+++ 1_Assignment/1_part/HeavyQueen.py
@@ -0,0 +1,5 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Simulates the Heavy Queens Problem.')
+parser.add_argument('num', help='The number of Queens')
+args = parser.parse_args()
