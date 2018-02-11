5decfeb46a6c95ac9a5e23e79bda3385d51cad7b
Myles Spencer <mspencer.mes@gmail.com>
2018-02-11 15:57:16 -0500

    Added move generation

--- 1_Assignment/1_part/HeavyQueen.py
+++ 1_Assignment/1_part/HeavyQueen.py
@@ -1,9 +1,9 @@
 import argparse, random
 
 class Board:
-    def __init__(self, size: int):
+    def __init__(self, size: int, board = []):
         self.size = size
-        self.board = []
+        self.board = board
 
     def __str__(self):
         board_string = str(self.board) + '\n\n'
@@ -18,8 +18,62 @@ class Board:
         return board_string
 
     def generate(self):
+        self.board = []
         for i in range(self.size):
             self.board.append(random.randint(0, self.size - 1))
+
+    def calculate_column_heuristic(self, column: int):
+        count = 0;
+        for i in range(self.size):
+            if self.board[column] == self.board[i] or abs(column - i) == abs(self.board[column] - self.board[i]):
+                count += 1
+
+        return count - 1
+
+    def calculate_heuristic(self):
+        count = 0
+        for i in range(self.size):
+            count += self.calculate_column_heuristic(i)
+
+        if count > 0:
+            return count + 10
+
+        return 0
+
+    def move(self, column, row):
+        if column < self.size and row < self.size:
+            self.board[column] = row
+
+    def copy(self):
+        return Board(self.size, list(self.board))
+
+    def generate_moves(self):
+        moves = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[j] != i:
+                    board = self.copy()
+                    cost = (abs(board.board[j] - i) ** 2) + 10
+                    board.move(j, i)
+                    move = Move(board, cost)
+                    moves.append(move)
+        return moves
+
+
+
+class Move:
+    def __init__(self, board: Board, cost: int):
+        self.board = board
+        self.cost = cost
+
+    def __str__(self):
+        return str(self.board) + '\nCost: ' + str(self.cost)
+
+    def total_cost():
+        return self.cost + self.board.calculate_heuristic()
+
+
+
 
 
 parser = argparse.ArgumentParser(description='Simulates the Heavy Queens Problem.')
@@ -28,8 +82,12 @@ parser.add_argument('algorithm', help='Which algorithm to use: 1 - A*, 2 - Hill 
 args = parser.parse_args()
 
 board = Board(args.N)
-board.generate();
+board.generate()
 print(board)
+print(board.calculate_heuristic())
+moves = board.generate_moves()
+for i in range(len(moves)):
+    print(moves[i])
 
 if args.algorithm == 1:
     pass
