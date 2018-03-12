f7581fae4bc9820736ab056d08c0d5dec757e03b
solashirai <solashakashirai@gmail.com>
2018-03-12 19:22:28 -0400

    add new directory for assignment 2 implement basics nodes for gibbs sampling

---
+++ 2_Assignment/1_part/gibbs.py
@@ -0,0 +1,93 @@
+from typing import Type, Sequence, Dict
+import random
+
+class GibbsNode:
+
+    #the actual value of the node is stored as self.value
+    #this actual value should be an int, starting at 0
+    #probability_dict should take tuples of integers as key, and have a list of probabilities as value
+    def __init__(self, value_names : Sequence[str], probability_dict,
+                 parents : Sequence['GibbsNode']):
+        self.value_names = value_names
+        self.p_dict = probability_dict
+        self.parents = parents
+        self.value = 0 #initialize value as 0 by default
+        self.max_value = len(self.value_names)-1
+
+    def get_string_value(self) -> str:
+        return self.value_names[self.value]
+
+    def get_current_value(self) -> int:
+        return self.value
+
+    def set_value_random(self):
+        self.value = random.randint(0, self.max_value)
+
+    def update_node(self):
+        #get values of parents
+        parent_values = tuple(map(lambda x: x.get_current_value, self.parents))
+        #get the probability distribution for the current configuration
+        probabilities = self.p_dict[parent_values]
+        randv = random.random()
+        ptotal = 0
+        #iterate through and add each probability, use this running total to determine the new value of this node
+        for i in range(0, len(probabilities)):
+            ptotal += probabilities[i]
+            if randv < ptotal:
+                self.value = i
+                break
+
+def main():
+    amenities = GibbsNode(["lots", "little"], {(): [0.3, 0.7]}, [])
+    neighborhood = GibbsNode(["bad", "good"], {(): [0.4, 0.6]}, [])
+    location = GibbsNode(["good", "bad", "ugly"], {
+        (0, 0): [0.3, 0.4, 0.3],
+        (0, 1): [0.8, 0.15, 0.05],
+        (1, 0): [0.2, 0.4, 0.4],
+        (1, 1): [0.5, 0.35, 0.15]
+    }, [amenities, neighborhood])
+    children = GibbsNode(["bad", "good"], {(0) : [0.6, 0.4], (1) : [0.3, 0.7]}, [neighborhood])
+    size = GibbsNode(["small", "medium", "large"], {(): [0.33, 0.34, 0.33]}, [])
+    schools = GibbsNode(["bad", "good"], {(0) : {0.7, 0.3}, (1) : {0.8, 0.2}}, [children])
+    age = GibbsNode(["old", "new"], {(0): [0.3, 0.7], (1) : {0.6, 0.4}, (2): {0.9, 0.1}}, [location])
+    price = GibbsNode(["cheap", "ok", "expensive"], {
+        (0, 0, 0, 0) : [0.5, 0.4, 0.1],
+        (0, 0, 0, 1) : [0.4, 0.45, 0.15],
+        (0, 0, 0, 2) : [0.35, 0.45, 0.2],
+        (0, 0, 1, 0) : [0.4, 0.3, 0.3],
+        (0, 0, 1, 1) : [0.35, 0.3, 0.35],
+        (0, 0, 1, 2) : [0.3, 0.25, 0.45],
+        (0, 1, 0, 0) : [0.45, 0.4, 0.15],
+        (0, 1, 0, 1) : [0.4, 0.45, 0.15],
+        (0, 1, 0, 2) : [0.35, 0.45, 0.2],
+        (0, 1, 1, 0) : [0.25, 0.3, 0.45],
+        (0, 1, 1, 1) : [0.2, 0.25, 0.55],
+        (0, 1, 1, 2) : [0.1, 0.2, 0.7],
+        (1, 0, 0, 0) : [0.7, 0.299, 0.001],
+        (1, 0, 0, 1) : [0.65, 0.33, 0.02],
+        (1, 0, 0, 2) : [0.65, 0.32, 0.03],
+        (1, 0, 1, 0) : [0.55, 0.35, 0.1],
+        (1, 0, 1, 1) : [0.5, 0.35, 0.15],
+        (1, 0, 1, 2) : [0.45, 0.4, 0.15],
+        (1, 1, 0, 0) : [0.6, 0.35, 0.05],
+        (1, 1, 0, 1) : [0.55, 0.35, 0.1],
+        (1, 1, 0, 2) : [0.5, 0.4, 0.1],
+        (1, 1, 1, 0) : [0.4, 0.4, 0.2],
+        (1, 1, 1, 1) : [0.3, 0.4, 0.3],
+        (1, 1, 1, 2) : [0.3, 0.3, 0.4],
+        (2, 0, 0, 0) : [0.8, 0.1999, 0.0001],
+        (2, 0, 0, 1) : [0.75, 0.24, 0.01],
+        (2, 0, 0, 2) : [0.75, 0.23, 0.02],
+        (2, 0, 1, 0) : [0.65, 0.3, 0.05],
+        (2, 0, 1, 1) : [0.6, 0.33, 0.07],
+        (2, 0, 1, 2) : [0.55, 0.37, 0.08],
+        (2, 1, 0, 0) : [0.7, 0.27, 0.03],
+        (2, 1, 0, 1) : [0.64, 0.3, 0.06],
+        (2, 1, 0, 2) : [0.61, 0.32, 0.07],
+        (2, 1, 1, 0) : [0.48, 0.42, 0.1],
+        (2, 1, 1, 1) : [0.41, 0.39, 0.2],
+        (2, 1, 1, 2) : [0.37, 0.33, 0.3]
+    }, [location, age, schools, size])
+
+if __name__ == "__main__":
+    main()
---
+++ 2_Assignment/README.md
@@ -0,0 +1,8 @@
+# Assignment 2
+
+Authors: 
+- Sola Shirai
+- Myles Spencer
+- Saahil Claypool
+
+https://docs.google.com/document/d/1UOQ0pIK3_Pk0NkF5mdgPGKvt5S7TWa01W25Wi-dCK_c/edit?usp=sharing
