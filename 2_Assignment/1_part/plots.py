3543d83b7d0a2db0795119eb3745adc9419d6861
saahilclaypool <saahil@claypools.org>
2018-03-14 14:08:35 -0400

    Added labels etc to plots
    note: There is a random chance some states are never
    set, so on a small number of iterations, or a high number
    of drops, the code will fail. Not sure if this is the fault
    of the plotting code or the gibbs dropping code.

--- 2_Assignment/1_part/plots.py
+++ 2_Assignment/1_part/plots.py
@@ -19,14 +19,14 @@ def batch_simulate(node_list: Sequence['gibbs.GibbsNode'], iterationNumbs: Seque
 
     for it in iterationNumbs: 
         # do without drops
-        drops = gibbs.simulate(node_list, it, 0)
+        drops = gibbs.simulate(node_list, it, dropnum)
         for q in drops: 
             drop_probs[q.get_name()][it] = q.get_independent_probability()
 
         # do with drops
         nodrops = gibbs.simulate(node_list, it, 0)
         for q in nodrops: 
-            nodrop_probs[q.get_name()] = q.get_independent_probability()
+            nodrop_probs[q.get_name()][it] = q.get_independent_probability()
     
     return {"drop": drop_probs, "nodrop": nodrop_probs}
 
@@ -49,19 +49,43 @@ def make_plots(probs):
                     y[state] = []
                 y[state].append(prob)
 
-        for k,v in y.items():
-            print(k,len(v))
         # for each query, make a plot
         for state, probs in y.items():
-            plt.plot(x, probs)
-        plt.show()
+            plt.plot(x, probs, label="{}".format(state))
+        plt.legend()
+        plt.title(query)
+        plt.xlabel("Iterations")
+        plt.ylabel("Probability")
+        plt.savefig("img/drop_{}.png".format(state))
+        plt.close()
+
+    for query, its in nodrops.items(): 
+        x = []
+        y = {}
+        for it, result in its.items():
+            print(result)
+            x.append(it)
+            for state, prob in result.items():
+                if(state not in y.keys()):
+                    y[state] = []
+                y[state].append(prob)
+
+        # for each query, make a plot
+        for state, probs in y.items():
+            plt.plot(x, probs, label="{}".format(state))
+        plt.legend()
+        plt.title(query)
+        plt.xlabel("Iterations")
+        plt.ylabel("Probability")
+        plt.savefig("img/nodrop_{}.png".format(state))
+        plt.close()
         
     pass
 
 def main():
     print("hello world")
     node_list = gibbs.setup()
-    probs = batch_simulate(node_list, range(40, 4020, 100), 50)
+    probs = batch_simulate(node_list, range(100, 4000, 100), 50)
     make_plots(probs)
     ## Plots: for each thing, 
 
